0ef41f1feaca24311c5f6bd4858df1479dd26fc7
Bruno Welber Pereira <bwelberp@everis.com>
2018-09-19 17:01:59 -0300

    instalar laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<main>
+    <h1>Pesquisa nacional de leitores de tela</h1>
+    <p>Bem-vindo blah, blah, blah...</p>
+    <button class="usa-button">Iniciar a pesquisa</button>
+</main>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Pesquisa nacional de leitores de tela</title>
+  <link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('stylesheets/pesquisa.css') }}">
+</head>
+<body>
+@yeld('content')
+<script src="assets/uswds-1.0.0/js/uswds.min.js"></script>
+</body>
+</html>
