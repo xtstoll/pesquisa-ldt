b0f955379242f23f9c3a6614874f20c371c6b814
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-20 17:32:39 -0300

    Atualizei perguntas da parte 5

--- resources/views/parte5.blade.php
+++ resources/views/parte5.blade.php
@@ -20,40 +20,29 @@
           <option value>Sem resposta</option>
           <option value="prefere-web-desktop">Computador / notebook</option>
           <option value="prefere-web-mobile">Celular / tablet</option>
-          <option value="prefere-web-nao">Não tenho uma preferência</option>
+          <option value="prefere-web-nao">Não tenho nenhuma preferência</option>
       </select>
 
-      <label for="mais-acessivel">5.2 Na sua opinião, você acha que a Internet ficou mais acessível depois que entrou em vigor a LBI (Lei Brasileira de Inclusão, nº 13.146/2015)?</label>
+      <label for="mais-acessivel">5.2 Na sua opinião, você acha que a Internet ficou mais acessível desde 2015?</label>
       <select name="mais-acessivel" id="mais-acessivel">
           <option value>Sem resposta</option>
           <option value="mais-acessivel-sim">Sim, ficou mais acessível</option>
           <option value="mais-acessivel-igual">Continua igual para mim</option>
           <option value="mais-acessivel-nao">Não, ficou menos acessível</option>
         </select>
-
-      <label for="setor-acessivel">5.3 Qual é o setor que possui sites mais acessíveis para você?</label>
-      <select name="setor-acessivel" id="setor-acessivel">
+        
+       <label for="lbi">5.3 Você ouviu falar da LBI (Lei Brasileira de Inclusão, nº 13.146/2015)?</label>
+       <select name="lbi" id="lbi">
           <option value>Sem resposta</option>
-          <option value="setor-acessivel-educacao">Educação</option>
-          <option value="setor-acessivel-entretenimento">Entretenimento</option>
-          <option value="setor-acessivel-financa">Finança</option>
-          <option value="setor-acessivel-restaurantes">Restaurantes</option>
-          <option value="setor-acessivel-seguros">Seguros</option>
-          <option value="setor-acessivel-turismo">Turismo</option>
-          <option value="setor-acessivel-varejo">Varejo</option>
-        </select>
+          <option value="lbi-sim">Sim</option>
+          <option value="lbi-nao">Não</option>
+       </select>        
 
-      <label for="setor-inacessivel">5.4 Qual é o setor que possui sites menos acessíveis para você?</label>
-      <select name="setor-inacessivel" id="setor-inacessivel">
-          <option value>Sem resposta</option>
-          <option value="setor-inacessivel-educacao">Educação</option>
-          <option value="setor-inacessivel-entretenimento">Entretenimento</option>
-          <option value="setor-inacessivel-financa">Finança</option>
-          <option value="setor-inacessivel-restaurantes">Restaurantes</option>
-          <option value="setor-inacessivel-seguros">Seguros</option>
-          <option value="setor-inacessivel-turismo">Turismo</option>
-          <option value="setor-inacessivel-varejo">Varejo</option>
-        </select>          
+      <label for="site-acessivel">5.4 Qual é o site mais acessível que você utiliza?</label>
+        <input id="site-acessivel" name="site-acessivel" type="text">
+
+      <label for="site-inacessivel">5.5 Qual é o site menos acessível que você utiliza?</label>
+        <input id="site-inacessivel" name="site-inacessivel" type="text">
 
         <button class="usa-button-secondary">Voltar</button>
         <button class="usa-button">Continuar</button>
