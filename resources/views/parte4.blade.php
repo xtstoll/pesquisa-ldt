17b629b568953d4134ddcffa489fd189df6f520c
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-25 18:00:02 -0300

    Editei 4.1, 4.2 e 4.3

--- resources/views/parte4.blade.php
+++ resources/views/parte4.blade.php
@@ -7,11 +7,12 @@
     <form class="usa-form-large">
       <fieldset>
 
-        <label for="braille-virtual">4.1 Você utiliza algum teclado virtual em braille no seu celular / tablet? <em>Por exemplo, Entrada via Braille do iOS ou o aplicativo Soft Braille Keyboard do Android.</em></label>    
+        <label for="braille-virtual">4.1 Você utiliza algum teclado virtual braille no seu celular / tablet? <em>Por exemplo, Entrada via Braille do iOS ou o aplicativo Soft Braille Keyboard do Android.</em></label>    
         <select name="braille-virtual" id="braille-virtual">
             <option value>Sem resposta</option>
             <option value="braille-virtual-sim">Sim</option>
-            <option value="braille-virtual-sim">Não</option>
+            <option value="braille-virtual-nao">Não</option>
+            <option value="braille-virtual-naosei">Não sei do que se trata</option>
         </select>
 
         <label for="braille-linha">4.2 Você utiliza alguma linha braille com seu leitor de tela?</label>    
@@ -19,13 +20,14 @@
             <option value>Sem resposta</option>
             <option value="braille-linha-sim">Sim</option>
             <option value="braille-linha-sim">Não</option>
+            <option value="braille-linha-naosei">Não sei do que se trata</option>
         </select>
 
         <label for="teclado-externo">4.3 Você utiliza algum teclado externo para navegar e digitar no seu celular / tablet?</label>    
         <select name="teclado-externo" id="teclado-externo">
             <option value>Sem resposta</option>
             <option value="teclado-externo-sim">Sim</option>
-            <option value="teclado-externo-sim">Não</option>
+            <option value="teclado-externo-nao">Não</option>
         </select>
 
         <label for="digitacao-voz">4.4 Com qual frequência você utiliza o recurso de digitação por voz no seu celular / tablet?</label>    
