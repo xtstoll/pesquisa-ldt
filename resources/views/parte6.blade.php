b52065c442ab3419a30f81fc1129958eb6139468
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-25 16:16:54 -0300

    Adicionei números às perguntas

--- resources/views/parte6.blade.php
+++ resources/views/parte6.blade.php
@@ -7,21 +7,21 @@
   <form class="usa-form-large">
     <fieldset>
 
-       <label for="premio">Quer ter a oportunidade de receber um prêmio para sua participação nesta pesquisa?</label>       
+       <label for="premio">6.1 Quer ter a oportunidade de receber um prêmio para sua participação nesta pesquisa?</label>       
        <select name="premio" id="premio">
           <option value>Sem resposta</option>
           <option value="premio-sim">Sim</option>
           <option value="premio-não">Não</option>
         </select>
 
-       <label for="publicamos">Quer receber um e-mail quando publicarmos os resultados da pesquisa?</label>       
+       <label for="publicamos">6.2 Quer receber um e-mail quando publicarmos os resultados da pesquisa?</label>       
        <select name="publicamos" id="publicamos">
           <option value>Sem resposta</option>
           <option value="publicamos-sim">Sim</option>
           <option value="publicamos-não">Não</option>
         </select>  
 
-        <label for="email">Qual o seu e-mail?</label>
+        <label for="email">6.3 Qual o seu e-mail?</label>
         <input id="email" name="email" type="email">             
 
         <button class="usa-button-secondary">Voltar</button>
