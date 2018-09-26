a1815d9cc42ed1328e6a12d55b05d1cc50b3c941
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-26 15:46:56 -0300

    Adicionei required e selecione

--- resources/views/parte6.blade.php
+++ resources/views/parte6.blade.php
@@ -11,28 +11,28 @@
 @else
 
 <label for="sorteio">Quer ter a oportunidade de receber um prêmio para sua participação nesta pesquisa?</label>       
-       <select name="sorteio" id="sorteio">
-          <option value>Sem resposta</option>
+       <select name="sorteio" id="sorteio" required>
+          <option value>Selecione</option>
           <option value="premio-sim">Sim</option>
           <option value="premio-não">Não</option>
         </select>
 
        <label for="publicarmos">Quer receber um e-mail quando publicarmos os resultados da pesquisa?</label>       
-       <select name="publicarmos" id="publicarmos">
-          <option value>Sem resposta</option>
+       <select name="publicarmos" id="publicarmos" required>
+          <option value>Selecione</option>
           <option value="publicamos-sim">Sim</option>
           <option value="publicamos-não">Não</option>
         </select>  
 @endif
 
         <label for="email">Qual o seu e-mail?</label>
-        <input id="email" name="email" type="email">             
+        <input id="email" name="email" type="email" required>             
 
         <label for="nome">Qual o seu nome?</label>
-        <input id="nome" name="nome" type="text">     
+        <input id="nome" name="nome" type="text" required>     
 
         <label for="pesquisa">O que você achou da pesquisa?</label>
-        <input id="pesquisa" name="pesquisa" type="text">  
+        <input id="pesquisa" name="pesquisa" type="text" required>  
         
         <button class="usa-button-secondary">Voltar</button>
         <button class="usa-button">Enviar</button>
