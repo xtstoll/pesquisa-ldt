eb43f6e91e432342848a0b59f9650bc40874c4b0
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-26 15:40:55 -0300

    Adicionei required, selecione e não sei

--- resources/views/parte4.blade.php
+++ resources/views/parte4.blade.php
@@ -8,31 +8,32 @@
       <fieldset>
 
         <label for="braille-virtual">4.1 Você utiliza algum teclado virtual braille no seu celular / tablet? <em>Por exemplo, Entrada via Braille do iOS ou o aplicativo Soft Braille Keyboard do Android.</em></label>    
-        <select name="braille-virtual" id="braille-virtual">
-            <option value>Sem resposta</option>
+        <select name="braille-virtual" id="braille-virtual" required>
+            <option value>Selecione</option>
             <option value="braille-virtual-sim">Sim</option>
             <option value="braille-virtual-nao">Não</option>
             <option value="braille-virtual-naosei">Não sei do que se trata</option>
         </select>
 
         <label for="braille-linha">4.2 Você utiliza alguma linha braille com seu leitor de tela?</label>    
-        <select name="braille-linha" id="braille-linha">
-            <option value>Sem resposta</option>
+        <select name="braille-linha" id="braille-linha" required>
+            <option value>Selecione</option>
             <option value="braille-linha-sim">Sim</option>
             <option value="braille-linha-sim">Não</option>
             <option value="braille-linha-naosei">Não sei do que se trata</option>
         </select>
 
         <label for="teclado-externo">4.3 Você utiliza algum teclado externo para navegar e digitar no seu celular / tablet?</label>    
-        <select name="teclado-externo" id="teclado-externo">
-            <option value>Sem resposta</option>
+        <select name="teclado-externo" id="teclado-externo" required>
+            <option value>Selecione</option>
             <option value="teclado-externo-sim">Sim</option>
             <option value="teclado-externo-nao">Não</option>
+            <option value="teclado-externo-naosei">Não sei do que se trata</option>
         </select>
 
         <label for="digitacao-voz">4.4 Com qual frequência você utiliza o recurso de digitação por voz no seu celular / tablet?</label>    
-        <select name="digitacao-voz" id="digitacao-voz">
-            <option value>Sem resposta</option>
+        <select name="digitacao-voz" id="digitacao-voz" required>
+            <option value>Selecione</option>
             <option value="digitacao-voz-sempre">Sempre</option>
             <option value="digitacao-voz-frequentemente">Frequentemente</option>
             <option value="digitacao-voz-asvezes">Às vezes</option>
