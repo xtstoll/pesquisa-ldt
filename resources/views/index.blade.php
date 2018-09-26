819761c1d07437fc4088c4864077e72bccd37280
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-26 13:58:14 -0300

    Atualizei a descrição

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <main>
     <h1>Pesquisa nacional de leitores de tela</h1>
-    <p>Bem-vindo blah, blah, blah, testing...</p>
+    <p>Bem vindo à Pesquisa Nacional de Leitores de Tela! É a primeira pesquisa nacional para usuários de leitores de tela.</p>
+    <p>Essa é a versão beta. Na última página da pesquisa inclua sua opinião sobre ela, por favor. Obrigado!</p>
     <button class="usa-button">Iniciar a pesquisa</button>
 </main>
 @endsection
