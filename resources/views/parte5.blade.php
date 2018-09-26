db4ac57a38152dbc52dd88857e0d83833dffb474
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-26 15:45:38 -0300

    Adicionei required e selecione

--- resources/views/parte5.blade.php
+++ resources/views/parte5.blade.php
@@ -8,33 +8,33 @@
     <fieldset>
 
       <label for="prefere-web">5.1 Você prefere navegar em uma página da Internet pelo computador / notebook ou pelo celular / tablet?</label>
-      <select name="prefere-web" id="prefere-web">
-          <option value>Sem resposta</option>
+      <select name="prefere-web" id="prefere-web" required>
+          <option value>Selecione</option>
           <option value="prefere-web-desktop">Computador / notebook</option>
           <option value="prefere-web-mobile">Celular / tablet</option>
           <option value="prefere-web-nao">Não tenho preferência</option>
       </select>
 
       <label for="mais-acessivel">5.2 Na sua opinião, você acha que a Internet ficou mais acessível desde 2015?</label>
-      <select name="mais-acessivel" id="mais-acessivel">
-          <option value>Sem resposta</option>
+      <select name="mais-acessivel" id="mais-acessivel" required>
+          <option value>Selecione</option>
           <option value="mais-acessivel-sim">Sim, ficou mais acessível</option>
           <option value="mais-acessivel-igual">Continua igual para mim</option>
           <option value="mais-acessivel-nao">Não, ficou menos acessível</option>
         </select>
         
        <label for="lbi">5.3 Você já ouviu falar da LBI (Lei Brasileira de Inclusão, nº 13.146/2015)?</label>
-       <select name="lbi" id="lbi">
-          <option value>Sem resposta</option>
+       <select name="lbi" id="lbi" required>
+          <option value>Selecione</option>
           <option value="lbi-sim">Sim</option>
           <option value="lbi-nao">Não</option>
        </select>        
 
       <label for="site-acessivel">5.4 Qual é o site mais acessível que você utiliza?</label>
-        <input id="site-acessivel" name="site-acessivel" type="text">
+        <input id="site-acessivel" name="site-acessivel" type="text" required>
 
       <label for="site-inacessivel">5.5 Qual é o site menos acessível que você utiliza?</label>
-        <input id="site-inacessivel" name="site-inacessivel" type="text">
+        <input id="site-inacessivel" name="site-inacessivel" type="text" required>
 
         <button class="usa-button-secondary">Voltar</button>
         <button class="usa-button">Continuar</button>
