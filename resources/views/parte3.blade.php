47d77b7e5b7e5869738897627738c2e95fd30df5
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-26 15:32:48 -0300

    Adicionei required, selecione e não utilize

--- resources/views/parte3.blade.php
+++ resources/views/parte3.blade.php
@@ -9,8 +9,8 @@
       <fieldset>
 
         <label for="navegador-desktop">3.1 Qual navegador você mais utiliza no seu computador / notebook?</label>    
-        <select name="navegador-desktop" id="navegador-desktop">
-            <option value>Sem resposta</option>
+        <select name="navegador-desktop" id="navegador-desktop" required>
+            <option value>Selecione</option>
             <option value="navegador-desktop-chrome">Chrome</option>
             <option value="navegador-desktop-edge">Edge</option>
             <option value="navegador-desktop-firefox">Firefox</option>
@@ -18,20 +18,22 @@
             <option value="navegador-desktop-opera">Opera</option>
             <option value="navegador-desktop-safari">Safari</option>
             <option value="navegador-desktop-outro">Outro</option>
-        </select>
+            <option value="navegador-desktop-naoutilizo">Não utilizo nenhum</option>
+       </select>
 
         <label for="navegador-mobile">3.2 Qual navegador você mais utiliza no seu celular / tablet?</label>    
-        <select name="navegador-mobile" id="navegador-mobile">
-            <option value>Sem resposta</option>        
+        <select name="navegador-mobile" id="navegador-mobile" required>
+            <option value>Selecione</option>
             <option value="navegador-mobile-chrome">Chrome</option>
             <option value="navegador-mobile-firefox">Firefox</option>
             <option value="navegador-mobile-safari">Safari</option>
             <option value="navegador-mobile-outro">Outro</option>
+            <option value="navegador-mobile-naoutilizo">Não utilizo nenhum</option>
         </select>
 
         <label for="nav-encontrar">3.3 Ao tentar encontrar informações em uma página da Internet, qual das seguintes opções você costuma utilizar?</label>
-        <select name="nav-encontrar" id="nav-encontrar">
-            <option value>Sem resposta</option>
+        <select name="nav-encontrar" id="nav-encontrar" required>
+            <option value>Selecione</option>
             <option value="nav-encontrar-cabecalhos">Navegar pelos cabeçalhos (tecla H ou números de 1 a 6)</option>
             <option value="nav-encontrar-pesquisa">Utilizar a pesquisa da página (Ctrl + F)</option>
             <option value="nav-encontrar-links">Navegar pelos links (Tab ou tecla K)</option>
@@ -40,8 +42,8 @@
         </select>
 
         <label for="nav-irpara">3.4 Com qual frequência você utiliza os links “ir para conteúdo” ou “ir para o menu” quando estão disponíveis em uma página?</label>
-        <select name="nav-irpara" id="nav-irpara">
-            <option value>Sem resposta</option>
+        <select name="nav-irpara" id="nav-irpara" required>
+            <option value>Selecione</option>
             <option value="nav-irpara-sempre">Sempre</option>
             <option value="nav-irpara-frequentemente">Frequentemente</option>
             <option value="nav-irpara-asvezes">Às vezes</option>
@@ -51,8 +53,8 @@
         </select>
 
         <label for="nav-marcas">3.5 Com qual frequência você utiliza a navegação pelas marcas / regiões (tecla D ou R) em uma página?</label>
-        <select name="nav-marcas" id="nav-marcas">
-            <option value>Sem resposta</option>
+        <select name="nav-marcas" id="nav-marcas" required>
+            <option value>Selecione</option>
             <option value="nav-marcas-sempre">Sempre</option>
             <option value="nav-marcas-frequentemente">Frequentemente</option>
             <option value="nav-marcas-asvezes">Às vezes</option>
@@ -62,8 +64,8 @@
         </select>
         
         <label for="nav-barra">3.6 Com qual frequência você utiliza as opções de aumentar/diminuir fonte ou alternar contraste, quando estão disponíveis em uma página?</label>
-        <select name="nav-barra" id="nav-barra">
-            <option value>Sem resposta</option>
+        <select name="nav-barra" id="nav-barra" required>
+            <option value>Selecione</option>
             <option value="nav-barra-sempre">Sempre</option>
             <option value="nav-barra-frequentemente">Frequentemente</option>
             <option value="nav-barra-asvezes">Às vezes</option>
@@ -73,8 +75,8 @@
         </select> 
         
         <label for="nav-teclas">3.7 Com qual frequência você utiliza as teclas de atalho de um site, quando estão disponíveis? Por exemplo, a tecla P para publicar um novo status no Facebook.</label>
-        <select name="nav-teclas" id="nav-teclas">
-            <option value>Sem resposta</option>
+        <select name="nav-teclas" id="nav-teclas" required>
+            <option value>Selecione</option>
             <option value="nav-teclas-sempre">Sempre</option>
             <option value="nav-teclas-frequentemente">Frequentemente</option>
             <option value="nav-teclas-asvezes">Às vezes</option>
@@ -84,8 +86,8 @@
         </select>
         
         <label for="nav-dificil">3.8 Em qual elemento você tem mais dificuldade de navegar?</label>
-        <select name="nav-dificil" id="nav-dificil">
-            <option value>Sem resposta</option>
+        <select name="nav-dificil" id="nav-dificil" required>
+            <option value>Selecione</option>
             <option value="nav-dificil-busca">Campo de busca inacessível ou inexistente</option>
             <option value="nav-dificil-cabecalhos">Cabeçalhos ausentes ou fora da ordem lógica</option>
             <option value="nav-dificil-excesso">Excesso de links ou itens de navegação</option>
