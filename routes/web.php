ecf8333e187768926e420c144ed92cf4ded96471
Bruno Welber Pereira <bwelberp@everis.com>
2018-09-26 08:45:43 -0300

    implementadas as rotas

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,32 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/parte1', function(){
+	return view('parte1');
+});
+
+Route::get('/parte2', function(){
+	return view('parte2');
+});
+
+Route::get('/parte3', function(){
+	return view('parte3');
+});
+
+Route::get('/parte4', function(){
+	return view('parte4');
+});
+
+Route::get('/parte5', function(){
+	return view('parte5');
+});
+
+Route::get('/parte6', function(){
+	return view('parte6');
+});
+
+
+
+
+
