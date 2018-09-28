3d7f77ec19834609bdee3c693ec6277507ea8340
isamadamasceno <43423317+isamadamasceno@users.noreply.github.com>
2018-09-28 19:52:24 -0300

    Correção js

--- resources/js/p1.js
+++ resources/js/p1.js
@@ -8,16 +8,18 @@ var dvb=document.getElementById("deficiencia-baixa-visao").checked;
 var dvCog=document.getElementById("deficiencia-cognitiva").checked;
 var dva=document.getElementById("deficiencia-auditiva").checked;
 		var dvm=document.getElementById("deficiencia-motora").checked;
+		var dvOutra=document.getElementById("deficiencia-outra").checked;
+		var dvNen=document.getElementById("deficiencia-nenhuma").checked;
 if((ano_nascimento<=1912) | (ano_nascimento<=2014))
 {
 parte1.ano_nascimento.focus();
-document.getElementById("anoNasc").innerHTML='<label for="ano_nascimento" id="anoNasc" role="alert"> Ano de Nascimento Inválido. -  1.1 Qual ano você nasceu?</label>';
+document.getElementById("ano").innerHTML='<label for="ano_nascimento" id="ano" role="alert"> Ano de Nascimento Inválido. -  1.1 Qual ano você nasceu?</label>';
 return false;
 }
 else if(genero=="")
 {
 parte1.genero.focus();
-document.getElementById("gn").innerHTML='<label for="genero" id="gn"  role="alert">Por favor, selecione uma opção abaixo. - 1.2 Qual é o seu gênero?</label>';
+document.getElementById("gen").innerHTML='<label for="genero" id="gen"  role="alert">Por favor, selecione uma opção abaixo. - 1.2 Qual é o seu gênero?</label>';
 return false;
 }
 else if(estado=="")
@@ -26,13 +28,13 @@ parte1.estado.focus();
 document.getElementById("est").innerHTML='<label for="estado" id="est"  role="alert">Por gentileza, selecione seu Estado. - 1.3 Em qual estado você mora?</label>';
 return false;
 }
-else if(escolaridade=="")
+else if(escolaridade=="")				
 {
 parte1.escolaridade.focus();
 document.getElementById("esc").innerHTML='<label for="escolaridade" id="esc" role="alert">Por gentileza, selecione uma opção abaixo. - 1.4 Qual o seu nível de escolaridade?</label>';
 return false;
 }
-else if((dvc==false) & (dvb==false) & (dva==false) & (dvCog==false) & (dva==false) & (dvm==false))
+else if((dvc==false) & (dvb==false) & (dva==false) & (dvCog==false) & (dva==false) & (dvm==false) & (dvOutra==false) & (dvNen==false)) 
 {
 parte1.deficiencia-cegueira.focus();
 document.getElementById("deficiencias").innerHTML='<label id="deficiencias" role="alert">Por gentileza, marque uma das opções abaixo. - 1.5 Qual(is) deficiência(s) você possui?</label>';
