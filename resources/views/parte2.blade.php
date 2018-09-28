1939c18973f7c3f819bf2610224dee60e45990cb
isamadamasceno <43423317+isamadamasceno@users.noreply.github.com>
2018-09-28 18:36:32 -0300

    adicionado ids nas labels e onclick no btn

--- resources/views/parte2.blade.php
+++ resources/views/parte2.blade.php
@@ -7,10 +7,10 @@
 <main>
     <h1>Parte 2: Leitores de tela</h1>
 
-    <form class="usa-form-large" action="{{ url('/parte2') }}" method="post">
+    <form name="parte2" class="usa-form-large" action="{{ url('/parte2') }}" method="post">
       <fieldset>
         
-        <label for="ldt-desktop1">2.1 Qual leitor de tela você mais utiliza para computador / notebook?</label>
+        <label for="ldt-desktop1" id="desc1">2.1 Qual leitor de tela você mais utiliza para computador / notebook?</label>
         <select name="ldt_desktop1" id="ldt-desktop1" required>
             <option value="">Selecione</option>
             <option value="chromevox">ChromeVox</option>
@@ -27,7 +27,7 @@
             <option value="naoutilizo">Não utilizo nenhum</option>
         </select>    
 
-        <label for="ldt-desktop2">2.2 Há outro leitor de tela que você mais utiliza para computador / notebook?</label>
+        <label for="ldt-desktop2" id="desc2">2.2 Há outro leitor de tela que você mais utiliza para computador / notebook?</label>
         <select name="ldt_desktop2" id="ldt-desktop2" required>
             <option value="">Selecione</option>
             <option value="chromevox">ChromeVox</option>
@@ -44,7 +44,7 @@
             <option value="naoutilizo">Não, só utilizo um</option>
         </select> 
 
-        <label for="ldt-atualizar">2.3 Quando foi a última vez que você atualizou seu leitor de tela para computador / notebook?</label>
+        <label for="ldt-atualizar" id="atualizar">2.3 Quando foi a última vez que você atualizou seu leitor de tela para computador / notebook?</label>
         <select name="ldt_atualizado" id="ldt-atualizar" required>
             <option value="">Selecione</option>
             <option value="dias">Há alguns dias</option>
@@ -54,7 +54,7 @@
             <option value="naosei">Não sei</option>            
         </select>
 
-        <label for="ldt-mobile">2.4 Qual leitor de tela você mais utiliza para celular / tablet?</label>
+        <label for="ldt-mobile" id="mobile">2.4 Qual leitor de tela você mais utiliza para celular / tablet?</label>
         <select name="ldt_mobile" id="ldt-mobile" required>
             <option value="">Selecione</option>
             <option value="mobileacessibility">Mobile Accessibility</option>
@@ -67,7 +67,7 @@
             <option value="naoutilizo">Não utilizo nenhum</option>
         </select> 
 
-        <label for="uso-recurso">2.5 Qual opção descreve como você utiliza o seu leitor de tela?</label>
+        <label for="uso-recurso" id="uso">2.5 Qual opção descreve como você utiliza o seu leitor de tela?</label>
         <select name="ldt_uso" id="uso-recurso" required>
             <option value="">Selecione</option>
             <option value="apenas-leitor">Utilizo somente a fala do leitor de tela.</option>
@@ -76,7 +76,7 @@
             <option value="apenas-visual">Utilizo somente os recursos visuais.</option>
         </select>
 
-        <label for="uso-dispositivo">2.6 Você utiliza mais computador / notebook ou do celular / tablet?</label>
+        <label for="uso-dispositivo" id="dispositivo">2.6 Você utiliza mais computador / notebook ou do celular / tablet?</label>
         <select name="dispositivo_usado" id="uso-dispositivo" required>
             <option value="">Selecione</option>
             <option value="mais-desktop">Utilizo mais o leitor de tela do computador / notebook.</option>
@@ -84,7 +84,7 @@
             <option value="mais-mobile">Utilizo mais o leitor de tela do celular / tablet.</option>
         </select>
 
-        <label for="aprendeu">2.7 Como você aprendeu a utilizar seu leitor de tela?</label>
+        <label for="aprendeu" id="aprende">2.7 Como você aprendeu a utilizar seu leitor de tela?</label>
         <select name="aprendeu" id="aprendeu" required>
             <option value="">Selecione</option>
             <option value="amigos">Com amigos</option>
@@ -94,18 +94,18 @@
             <option value="sozinho">Sozinho</option>
         </select>
 
-        <label for="pagar">2.8 Você acha que vale a pena pagar por um leitor de tela?</label>
+        <label for="pagar" id="pg">2.8 Você acha que vale a pena pagar por um leitor de tela?</label>
         <select name="ldt_pagar" id="pagar" required>
             <option value="">Selecione</option>
             <option value="sim">Sim</option>
             <option value="nao">Não</option>
         </select>    
         
-        <label for="pagar-porque">2.9 Explique o motivo da resposta acima.</label>
+        <label for="pagar-porque" id="pgto">2.9 Explique o motivo da resposta acima.</label>
         <input id="pagar-porque" name="ldt_pagar_motivo" type="text" maxlength="250" placeholder="Motivo da resposta acima" required>        
 
         <button class="usa-button-secondary">Voltar</button>
-        <button class="usa-button" type="submit" >Continuar</button>
+        <button class="usa-button" type="button" onclick="validarP2();">Continuar</button>
        
        </fieldset>   
     </form>
