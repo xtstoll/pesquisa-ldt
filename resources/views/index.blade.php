d8932f103520b4edb5ec1e54a37b1a0cb012abfb
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-29 19:07:45 -0300

    revisei o texto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,9 @@
 		<div class="usa-width-one-whole">
 			
 		    <h1>Pesquisa Nacional de Leitores de Tela</h1>		    
-		    	<p>Bem vindos à primeira Pesquisa exclusivamente Nacional sobre usos e costumes de leitores de tela por usuários cegos e de baixa visão.</p>
+		    	<p>Bem vindos à primeira pesquisa nacional sobre usos e costumes de leitores de tela por usuários cegos e de baixa visão.</p>
 				<p>Estamos neste momento na versão beta, e temos o maior prazer de poder contar com vocês, escolhidos a dedo, para nos ajudar com críticas e sugestões, a fim de tornar a pesquisa o mais completa possível.</p>
-				<p>Na última página da pesquisa temos um espaço para que vocês possam deixar seu feedback e nos ajudar com este trabalho que, esperamos, sirva para conhecer as tendências e necessidades dos deficientes brasileiros.</p>
+				<p>Na última página da pesquisa temos um espaço para que vocês possam deixar seu feedback e nos ajudar com este trabalho que, esperamos, sirva para conhecer as tendências e necessidades dos brasileiros com deficiência.</p>
 				<p>A nossa equipe agradece a todos os beta testers pela colaboração!</p>		    
 		   
 		    <button class="usa-button">Iniciar a pesquisa</button>
