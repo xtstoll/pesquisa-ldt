90877a4967567766da199afa8b3a2f811662a4c6
isamadamasceno <43423317+isamadamasceno@users.noreply.github.com>
2018-09-29 13:31:06 -0300

    Adicionado os caminhos do js de validação das telas 1 ao 6.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>@yield('pageTitle') - Pesquisa nacional de leitores de tela</title>
+<script src="js/validarP1.js"></script>
+<script src="js/validarP2.js"></script>
+<script src="js/validarP3.js"></script>
+<script src="js/validarP4.js"></script>
+<script src="js/validarP5.js"></script>
+<script src="js/validarP6.js"></script>
   <link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
   <link rel="stylesheet" href="{{ asset('stylesheets/pesquisa.css') }}">
 </head>
