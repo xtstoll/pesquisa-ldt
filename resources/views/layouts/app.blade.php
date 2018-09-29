ce0f94662c287b4421fbea9e1b2c058eaa7fb677
Cristina Stoll <cristina.stoll@gmail.com>
2018-09-29 15:54:54 -0300

    Mudei o caminho js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,12 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>@yield('pageTitle') - Pesquisa nacional de leitores de tela</title>
-<script src="js/validarP1.js"></script>
-<script src="js/validarP2.js"></script>
-<script src="js/validarP3.js"></script>
-<script src="js/validarP4.js"></script>
-<script src="js/validarP5.js"></script>
-<script src="js/validarP6.js"></script>
+  <script src="js/validarP1.js"></script>
+  <script src="js/validarP2.js"></script>
+  <script src="js/validarP3.js"></script>
+  <script src="js/validarP4.js"></script>
+  <script src="js/validarP5.js"></script>
+  <script src="js/validarP6.js"></script>
   <link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
   <link rel="stylesheet" href="{{ asset('stylesheets/pesquisa.css') }}">
 </head>
@@ -19,6 +19,6 @@
     <p>Há uma dúvida? <a href="mailto:cristina.henrietta.stoll@everis.com?subject=Pesquisa LDT">Entre em contato por e-mail</a>.</p>
     <p>Copyright 2018 &copy; everis</p>
 </footer>
-<script src="assets/uswds-1.0.0/js/uswds.min.js"></script>
+<script src="{{ asset('uswds-1.6.8/js/uswds.min.js') }}"></script>
 </body>
 </html>
