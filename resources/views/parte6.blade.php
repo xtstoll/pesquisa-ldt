9db1a22f2412ddecc71b8b3379bd40d29b9fcb47
isamadamasceno <43423317+isamadamasceno@users.noreply.github.com>
2018-09-29 13:20:43 -0300

    alterações
    Alterações refletindo a mesma dos arquivos anteriores

--- resources/views/parte6.blade.php
+++ resources/views/parte6.blade.php
@@ -3,23 +3,23 @@
 @section('pageTitle', 'Parte 6')
 
 @section('content')
-<main>
+
   <h1>Parte 6: Conclusão</h1>
 
-  <form class="usa-form-large" action="{{ url('/parte6') }}" method="post" >
+  <form name="parte6" class="usa-form-large" action="{{ url('/parte6') }}" method="post" >
     <fieldset>
 
 @if(false)
 @else
 
-<label for="sorteio">Quer ter a oportunidade de receber um prêmio para sua participação nesta pesquisa?</label>       
+<label for="sorteio" id="Sorteio">Quer ter a oportunidade de receber um prêmio para sua participação nesta pesquisa?</label>       
        <select name="sorteio" id="sorteio" required>
           <option value="">Selecione</option>
           <option value="sim">Sim</option>
           <option value="não">Não</option>
         </select>
 
-       <label for="publicarmos">Quer receber um e-mail quando publicarmos os resultados da pesquisa?</label>       
+       <label for="publicarmos" id="Publicarmos">Quer receber um e-mail quando publicarmos os resultados da pesquisa?</label>       
        <select name="publicada" id="publicarmos" required>
           <option value="">Selecione</option>
           <option value="sim">Sim</option>
@@ -27,17 +27,17 @@
         </select>  
 @endif
 
-        <label for="email">Qual o seu e-mail?</label>
-        <input id="email" name="email" type="email" maxlength="100" required>             
+        <label for="email" id="Email">Qual o seu e-mail?</label>
+        <input id="email" name="email" onblur="validarMail(parte6.email);"  type="email" maxlength="100" required>             
 
-        <label for="nome">Qual o seu nome?</label>
+        <label for="nome" id="Nome">Qual o seu nome?</label>
         <input id="nome" name="nome" type="text" required>     
 
-        <label for="pesquisa">O que você achou da pesquisa?</label>
+        <label for="pesquisa" id="Pesquisa">O que você achou da pesquisa?</label>
         <input id="pesquisa" name="opniao" type="text" required>  
         
         <button class="usa-button-secondary">Voltar</button>
-        <button class="usa-button" type="submit" >Enviar</button>
+        <button class="usa-button" type="button" onclick="validarP6();">Enviar</button>
 
      </fieldset>      
   </form>
