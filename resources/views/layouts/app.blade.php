f5e99dcd2a21ebd51f2957f978be1325f3fe2c33
isamadamasceno <43423317+isamadamasceno@users.noreply.github.com>
2018-09-30 09:37:15 -0300

    adicionado caminho do js chk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,119 +1,21 @@
-@extends('layouts.app')
-
-@section('pageTitle', 'Parte 1')
-
-@section('content')
-<main id="main-content">
-	
-	<section class="usa-grid usa-section">
-
-		<div class="usa-width-one-whole">
-
-			<h1>Parte 1: Sobre você</h1>
-
-			    <form name="parte1" class="us	a-form-large" action="{{ url('/parte1') }}" method="post">     
-			      <fieldset>
-			        
-			        <label for="anonascimento" id="anoNascimento">1.1 Qual ano você nasceu?</label>
-			        <input id="anonascimento" name="ano_nascimento" type="number" min="1918" max="2018" placeholder="Ano de nascimento" required>
-			
-			        <label for="genero" id="Genero">1.2 Qual é o seu gênero?</label>
-			        <select name="genero" id="genero" required>
-			        	<option value="">Selecione</option>
-			        	<option value="homem">Homem</option>
-			        	<option value="mulher">Mulher</option>
-			        	<option value="outro">Outro</option>
-			        	<option value="semresposta">Não quero responder</option>
-			        </select>
-			
-			        <label for="estado" id="Estado">1.3 Em qual estado você mora?</label>
-			        <select name="estado" id="estado" required>
-			        	<option value="">Selecione</option>
-			        	<option value="AC">Acre</option>
-			        	<option value="AL">Alagoas</option>
-			        	<option value="AP">Amapá</option>
-			        	<option value="AM">Amazonas</option>
-			        	<option value="BA">Bahia</option>
-			        	<option value="CE">Ceará</option>
-			        	<option value="DF">Distrito Federal</option>
-			        	<option value="ES">Espírito Santo</option>
-			        	<option value="GO">Goiás</option>
-			        	<option value="MA">Maranhão</option>
-			        	<option value="MT">Mato Grosso</option>
-			        	<option value="MS">Mato Grosso do Sul</option>    	
-			        	<option value="MG">Minas Gerais</option>
-			        	<option value="PA">Pará</option>
-			        	<option value="PB">Paraíba</option>   
-			        	<option value="PR">Paraná</option>
-			        	<option value="PE">Pernambuco</option>
-			        	<option value="PI">Piauí</option> 
-			        	<option value="RJ">Rio de Janeiro</option>
-			        	<option value="RN">Rio Grande do Norte</option>
-			        	<option value="RS">Rio Grande do Sul</option> 
-			        	<option value="RO">Rondônia</option>
-			        	<option value="RR">Roraima</option>
-			        	<option value="RS">Santa Catarina</option> 
-			        	<option value="SP">São Paulo</option>
-			        	<option value="SE">Sergipe</option>
-			        	<option value="TO">Tocantins</option>
-			        </select>    
-			
-			        <label for="escolaridade" id="Escolaridade">1.4 Qual o seu nível de escolaridade?</label>
-			        <select name="escolaridade" id="escolaridade">
-			        	<option value="">Selecione</option>
-			        	<option value="ef-incompleto">Fundamental incompleto</option>
-			        	<option value="ef-completo">Fundamental completo</option>
-			        	<option value="em-incompleto">Ensino médio incompleto</option>
-			        	<option value="em-completo">Ensino médio completo</option>
-			        	<option value="es-incompleto">Superior incompleto</option>
-			        	<option value="es-completo">Superior completo</option>
-			        	<option value="pg">Pós-graduação</option>	
-						<option value="mestrado">Mestrado</option>
-						<option value="doutorado">Doutorado</option>
-			        	<option value="semresposta">Não quero responder</option>
-			        </select>
-			
-			    	<label id="Deficiencias">1.5 Qual(is) deficiência(s) você possui?</label>
-			    	 <ul class="usa-unstyled-list">
-			    	    <li>
-			    	      <input id="deficienciacegueira" role="checkbox" type="checkbox" name="deficiencia[]" value="cegueira" onclick="CHK();" onKeyPress="CHK();"  aria-label="não selecionado cegueira">
-			    	      <label for="deficienciacegueira">Cegueira</label>
-			    	    </li>
-			    	    <li>
-			    	      <input id="deficienciabaixavisao" type="checkbox" name="deficiencia[]" value="baixa-visao" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="baixa visão não selecionado">
-			    	      <label for="deficienciabaixavisao">Baixa visão</label>
-			    	    </li>
-			    	    <li>
-			    	      <input id="deficienciacognitiva" type="checkbox" name="deficiencia[]" value="cognitiva" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="deficiência coignitiva não selecionado">
-			    	      <label for="deficienciacognitiva">Cognitiva</label>
-			    	    </li> 
-			    	    <li>
-			    	      <input id="deficienciaauditiva" type="checkbox" name="deficiencia[]" value="auditiva" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="deficiência auditiva não selecionado">
-			    	      <label for="deficienciaauditiva">Auditiva</label>
-			    	    </li>
-			    	    <li>
-			    	      <input id="deficienciamotora" type="checkbox" name="deficiencia[]" value="motora" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="deficiência motora não selecionado">
-			    	      <label for="deficienciamotora">Motora</label>
-			    	    </li> 
-			            <li>
-			              <input id="deficienciaoutra" type="checkbox" name="deficiencia[]" value="outra" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="outra deficiência  não selecionado">
-			              <label for="deficienciaoutra">Outra</label>
-			            </li>  
-			    	    <li>
-			    	      <input id="deficiencianenhuma" type="checkbox" name="deficiencia[]" value="nenhuma" role="checkbox" onclick="CHK();" onKeyPress="CHK();" aria-label="nenhuma deficiência não selecionado">
-			    	      <label for="deficiencianenhuma">Nenhuma</label>
-			    	    </li>            
-			    	  </ul>
-			
-			        <button class="usa-button" type="button"  onclick="validarP1();">Continuar</button>
-		       
-		       </fieldset>      
-		    </form>
-
-
-			
-			</div>
-    	</section>
-</main>
-
-@endsection
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>@yield('pageTitle')Pesquisa nacional de leitores de tela</title>
+  <script src="js/validarP1.js"></script>
+  <script src="js/validarP2.js"></script>
+  <script src="js/validarP3.js"></script>
+  <script src="js/validarP4.js"></script>
+  <script src="js/validarP5.js"></script>
+  <script src="js/validarP6.js"></script>
+    <script src="js/CHK.js"></script>
+  <link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
+  <link rel="stylesheet" href="css/pesquisa.css">
+</head>
+<body>
+@yield('content')
+<script src="{{ asset('uswds-1.6.8/js/uswds.min.js') }}"></script>
+</body>
+</html>
