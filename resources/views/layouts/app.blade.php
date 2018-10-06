04c251f67cf15b02ac6de730317078f2b7126616
Cristina Stoll <cristina.stoll@gmail.com>
2018-10-06 09:20:56 -0300

    Adicionei script do Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('pageTitle')Pesquisa nacional de leitores de tela</title>
-  <script src="js/validarP1.js"></script>
-  <script src="js/validarP2.js"></script>
-  <script src="js/validarP3.js"></script>
-  <script src="js/validarP4.js"></script>
-  <script src="js/validarP5.js"></script>
-  <script src="js/validarP6.js"></script>
-  <script src="js/CHK.js"></script>
-  <link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
-  <link rel="stylesheet" href="css/pesquisa.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>@yield('pageTitle')Pesquisa nacional de leitores de tela</title>
+	<script src="js/validarP1.js"></script>
+	<script src="js/validarP2.js"></script>
+	<script src="js/validarP3.js"></script>
+	<script src="js/validarP4.js"></script>
+	<script src="js/validarP5.js"></script>
+	<script src="js/validarP6.js"></script>
+	<script src="js/CHK.js"></script>
+	<link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
+	<link rel="stylesheet" href="css/pesquisa.css">
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127040926-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		
+		gtag('config', 'UA-127040926-1');
+	</script>
+
 </head>
 <body>
 @yield('content')
