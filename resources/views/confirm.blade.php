aac619f1616f1c14baf023c2d381a4b53fae6221
Cristina Stoll <cristina.stoll@gmail.com>
2018-10-07 12:13:38 -0300

    Tirei final e deixe confirm

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -5,14 +5,13 @@
 @section('content')
 
 <main id="main-content">
-	<section class="usa-grid usa-section">
 		<div class="usa-width-one-whole">			
 			
 			<h1>Obrigado pela participação!</h1>
-			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato caso a gente tenha alguma dúvida.</p>		
+			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato por e-mail no caso a gente tenha alguma dúvida.</p>
+			<p><em>Equipe de acessibilidade everis Brasil</em></p>  		
 		
 		</div>    	
-    </section>
 </main>
 
 @endsection
