5f53601da55e04ee6b5244a0c2f6bf736e371fc4
Cristina Stoll <cristina.stoll@gmail.com>
2018-10-07 19:32:43 -0300

    Corrigi português

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -8,7 +8,7 @@
 		<div class="usa-width-one-whole">			
 			
 			<h1>Obrigado pela participação!</h1>
-			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato por e-mail no caso a gente tenha alguma dúvida.</p>
+			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato por e-mail no caso que a gente tenha alguma dúvida.</p>
 			<p><em>Equipe de acessibilidade everis Brasil</em></p>  		
 		
 		</div>    	
