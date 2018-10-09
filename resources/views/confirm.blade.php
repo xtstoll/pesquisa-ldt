fbded9dbaf58362713564f22f556cc49f8329d99
Cristina Stoll <cristina.stoll@gmail.com>
2018-10-09 14:39:32 -0300

    Marina correção

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -8,7 +8,7 @@
 		<div class="usa-width-one-whole">		
 			
 			<h1>Obrigado pela participação!</h1>
-			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato por e-mail no caso que a gente tenha alguma dúvida.</p>
+			<p>Sua pesquisa foi concluída com sucesso! Vamos entrar em contato por e-mail caso tenhamos alguma dúvida. Se você tiver alguma dúvida sobre a pesquisa, <a href="mailto:cristina.henrietta.stoll@everis.com?subject=Pesquisa%20LDT%20-%20Tela%20Inicial">entre em contato por e-mail conosco</a>.</p>
 			<p><em>Equipe de acessibilidade everis Brasil</em></p>  		
 		
 		</div>  
