69c344d4554d27f92f45934f1ccefe83c4c1e146
Cristina Stoll <cristina.stoll@gmail.com>
2018-10-10 13:42:43 -0300

    Redes sociais

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,19 @@
 	<script src="js/CHK.js"></script>
 	<link rel="stylesheet" href="{{ asset('uswds-1.6.8/css/uswds.min.css') }}">
 	<link rel="stylesheet" href="css/pesquisa.css">
-	<link rel="image_src" href="images/logo_pesquisa-ldt.png" alt="Logo da pesquisa brasileira sobre o uso de leitores de tela"/>	
-	
+	<link rel="image_src" href="images/logo_pesquisa-ldt.jpg" alt="Logo da pesquisa brasileira sobre o uso de leitores de tela"/>
+<!-- 	Facebook -->
+	<meta property="og:url" content="http://estudoinclusivo.com.br" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Pesquisa brasileira sobre o uso de leitores de tela" />
+	<meta property="og:description" content="Contribua para uma web mais inclusiva participando dessa pesquisa! Só leva de 15 a 20 min para preencher, e os dados coletados serão disponibilizados para todos." />
+	<meta property="og:image" content="images/logo_pesquisa-ldt.jpg" alt="Logo da pesquisa brasileira sobre o uso de leitores de tela" />	
+	<meta name="twitter:card" content="summary" />
+<!-- Twitter -->
+	<meta name="twitter:title" content="Pesquisa brasileira sobre o uso de leitores de tela" />
+	<meta name="twitter:description" content="Contribua para uma web mais inclusiva participando dessa pesquisa! Só leva de 15 a 20 min para preencher, e os dados coletados serão disponibilizados para todos." />
+	<meta name="twitter:image" content="images/logo_pesquisa-ldt.jpg" />
+
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127040926-1"></script>
 		<script>
